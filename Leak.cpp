d9ac3ace5a0002ea5366845624b51da3a803325b
APeshko <m2415803@edu.misis.ru>
2025-04-15 19:10:20 +0300

    Update Leak.cpp

--- Leak.cpp
+++ Leak.cpp
@@ -3,6 +3,7 @@
 void memory_leak() {
     for (int i = 0; i < 100; ++i) {
         int* arr = new int[100]; // Утечка - нет соответствующего delete[]
+        arr[0] = i; 
     }
     std::cout << "creat obj" << std::endl;
 }
